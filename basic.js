3f21450063b3d8cceb1e98a49a7999037ee5449d
Bryan Makini <36193690+bryanmakini@users.noreply.github.com>
2020-06-30 18:04:05 +0300

    Basic Js
    see inside

---
+++ basic.js
@@ -0,0 +1,64 @@
+/*var num =-10;
+while(num<=19){
+    console.log(num);
+    num++;
+}
+
+var even =10;
+while(even<=40){
+    if(even%2===0){
+        console.log(even);
+    }
+    even++
+} 
+var odd= 301;
+while(odd<=333){
+    if(odd%2 ==1){
+        console.log(odd);
+    }
+    odd++;
+}
+
+var both =5;
+while(both<=50){
+    if(both%3 ===0 || both%5=== 0){
+        console.log(both);
+
+    }
+    both++;
+}*/
+
+var answer= prompt("Are we there yet?");
+ while(answer!= "yeah" && answer!= "yes"){
+    var answer= prompt("Are we there yet?");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
